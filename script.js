7878398779df9a91fd5aa7e35db3070dab09ed6a
mhammjr <69519372+mhammjr@users.noreply.github.com>
2020-09-15 21:42:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let userNum = prompt('Please enter any positive number');
 
 for (num = 1; num <= userNum; num++) {
     if (num % 3 === 0 && num % 5 != 0) {
-        console.log('fizz');
+        console.log('fizz')
     } else if (num % 5 === 0 && num % 3 != 0) {
         console.log('buzz')
     } else if (num % 3 === 0 && num % 5 === 0) {
